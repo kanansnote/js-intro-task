c4c31a5ba143a8b394bb2314e6fe9846cd14a704
kanan <kanansnote@gmail.com>
2024-05-13 19:46:54 +0400

    Added the 8th solved task.

--- src/js/script.js
+++ src/js/script.js
@@ -86,3 +86,83 @@
 
 // x = 13.45;
 // console.log(x.toFixed(1));
+
+
+// ilk indeks? start r?q?m ver?r?k ?lav? üstün? 1 g?lm?k,
+// maksimum neç?y?d?k hesablanma h?ddini qeyd olunmaqla
+
+// let arr = [1,3,6,8,1,57,87];
+// for (let i=4; i <=arr.length-1; i ++) {
+//   console.log("salam, indexin deyeri budur:", arr[i]);
+// }
+
+// let arr = [1,3,6,8,1,57,87];
+// for (let i=4; i <=15; i ++) {
+//   console.log("salam, indexin deyeri budur:", i)
+// }
+
+// let i=0;
+// while (i<=10){
+//   console.log("salam");
+//   i++;
+// }
+
+// let i=0;
+// while (i<=arr.length-1){
+//   console.log(arr[i]);
+//   i++;
+// }
+
+// let i=0;
+
+// do {
+// console.log("salam", i);
+// i++
+
+// } while (i>10);
+
+
+// for (const  iterrator of arr) {
+//   console.log (iterrator)
+// }
+
+
+//  adini v? soyadini consola çixart
+
+//  let person ={
+//    name: "Rahib",
+//    surname: "Rzayev",
+//  };
+//  for (const key in person {
+//    console.log(key.person[key])
+//  }
+
+// Task 8
+
+// function longestWord(text) {
+//     const arr=text.split(' ');
+//     console.log(arr);
+//     let result=arr[0];
+
+//     for (let i=0; i < arr.length; i++) {
+//         if (result.length < arr[i].length) {
+//             result=arr[i];
+//         }
+//     }
+
+//     console.log(result);
+// }
+
+// longestWord('Web Development Tutorial');
+
+// Task 9
+
+// const arr=[1, 2, 3, 4];
+
+// const arr2=[...arr];
+
+// const text='1, 2, 3, 4';
+
+// console.log(arr == arr2);
+// console.log(arr == text);
+// console.log(arr2 == text);
